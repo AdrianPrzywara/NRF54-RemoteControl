a2ce3461a422e4654a609019feb91aa63890e053
Adrian Przywara <adrprzywara@gmail.com>
2026-03-09 19:34:18 +0100

    Led switching using interrupt from button

--- src/main.c
+++ src/main.c
@@ -2,9 +2,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-/*  500 msec */
-#define SLEEP_TIME_MS 500
-
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 #define SW0_NODE DT_ALIAS(sw0)
@@ -12,11 +9,19 @@
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
+/* Button callback data */
+static struct gpio_callback button_cb_data;
+
+static void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
+
+static void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	gpio_pin_toggle_dt(&led);
+}
+
 int main(void)
 {
-	int ret;
-	bool led_state = true;
-
+	/* Initialization */
 	if (!gpio_is_ready_dt(&led))
 	{
 		return -1;
@@ -25,38 +30,20 @@ int main(void)
 	{
         return -1;
 	}
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0)
+	if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
 	{
 		return -1;
 	}
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-    if (ret != 0)
+    if (gpio_pin_configure_dt(&button, GPIO_INPUT) < 0)
 	{
         return -1;
     }
-
-
-	while (1) {
-		int button_state = gpio_pin_get_dt(&button);
-
-		if (0 > button_state)
-		{
-			return -1;
-		}
-		else if (button_state > 0)
-		{
-			ret = gpio_pin_toggle_dt(&led);
-			if (ret < 0) {
-				return -1;
-			}
-			led_state = !led_state;
-			k_msleep(SLEEP_TIME_MS);
-		}
-		else
-		{
-			/* Button not active*/
-		}
+	if (gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_FALLING) < 0)
+	{
+		return -1;
 	}
+	gpio_init_callback(&button_cb_data, &button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+
 	return 0;
 }
